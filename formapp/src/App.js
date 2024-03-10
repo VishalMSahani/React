3cc9ef359f66344cf6cce5e7d5ce903392d1b584
VishalMSahani <vishalsahani0808@gmail.com>
2024-03-10 13:51:01 +0530

    completed password generator

--- formapp/src/App.js
+++ formapp/src/App.js
@@ -1,4 +1,4 @@
-import {useState}  from 'react';
+import {useState, useEffect, useRef}  from 'react';
 import { useCallback } from 'react';
 import './App.css';
 
@@ -8,27 +8,37 @@ function App() {
   const [charAllowed, setCharallowed ] = useState(false);
   const [length , setLength] = useState(8);
   const  [password, setPassword] = useState("");
+  const passwordRef = useRef(null) ;
+
+  const cpoyPassword = useCallback(()=>{
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+
+  },[password]);
+
   
-  const passowrdGenerator = useCallback(() => {
+  const passwordgenerator = useCallback(() => {
 
     let pass = " "
     let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     if (numberAllowed) 
       str  += "0123456789"
     if (charAllowed)
-       str += "!@#$%^&*()-_+=[]{};:'"
+       str += "!@#$%^&*"
 
-       for (let i = 0; i <= length; i++) {
+       for (let i = 1; i <= length; i++) {
         let char = Math.floor(Math.random() * str.length + 1)
-        pass = str.charAt(char);
+        pass += str.charAt(char);
         
        }
-     
+    setPassword(pass);
 
       
     }, [numberAllowed, charAllowed, length, setPassword]);
 
-  
+  useEffect(() => {
+    passwordgenerator()
+  }, [numberAllowed, charAllowed, length, passwordgenerator])
 
 
   return (
@@ -36,20 +46,20 @@ function App() {
       <h2 className='text-white text-4xl'>Password Generator</h2>
       <div className='mt-10'>
         <div className=' flex w-[270px] h-auto p-1 border-2 border-gray-400 rounded-lg'>
-        <input 
+        <input value={password} ref={passwordRef}  readOnly type="text" 
         className=' max-w-[200px] h-auto p-4 border-2 rounded-lg'
-        type="text" id='name' placeholder="password"/>
-        <button className='px-2 text-white text-lg '>Copy</button>
+         id='name' placeholder="password"/>
+        <button onClick={cpoyPassword} className='px-2 text-white text-lg'>Copy</button>
         </div>
         <div className='mt-4 flex justify-center gap-3 text-white font-semibold'>
-          <input type="range" name="" id="" min="8" max="30" />
-          <label>Length : 8</label>
+          <input value={length} onChange={(e) => {setLength(e.target.value)}} type="range" name="" id="" min="1" max="30" />
+          <label>Length :{length}</label>
         </div>
         <div className='mt-2 text-lg flex justify-center gap-3 text-white font-semibold '>
-          <input type="checkbox" name="" id="num" />
+            <input type="checkbox" defaultChecked={numberAllowed} onChange={() => setNumberAllowed((prev) => !prev)} name="" id="num"  />
           <label htmlFor="num">Numbers</label>
-          <input type="checkbox" name="" id="char" />
-          <label htmlFor="char">Numbers</label>
+          <input defaultChecked={charAllowed} onChange={() => setCharallowed((prev) => !prev)} type="checkbox" name="" id="char" />
+          <label htmlFor="char">Spl Char.</label>
         </div>
 
 
